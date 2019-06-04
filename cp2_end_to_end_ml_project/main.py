28d2acffd4886d802e01c22ab5ea493d7a3ff41f
arcia <andy-wen@hotmail.com>
2019-06-03 22:43:19 -0700

    from home

--- cp2_end_to_end_ml_project/main.py
+++ cp2_end_to_end_ml_project/main.py
@@ -3,10 +3,11 @@ import os
 import tarfile
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib
+import numpy as np
 from six.moves import urllib
+from zlib import crc32
 
-# matplotlib.use('TkAgg')
+
 #%%
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -37,3 +38,29 @@ housing.head()
 #%%
 housing.hist(bins=50, figsize=(20, 15))
 plt.show()
+
+#%% Create a test
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data(id_column)
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+housing_with_id = housing.reset_index()
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+
+housing_with_id['id'] = housing['longitude'] * 1000 + housing['latitude']
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, 'id')
