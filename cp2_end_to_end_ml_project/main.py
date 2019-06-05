a3b33593d3e45e4246b4faa7b3e3665325adcbfa
arcia <andy-wen@hotmail.com>
2019-06-04 23:27:47 -0700

    from home

--- cp2_end_to_end_ml_project/main.py
+++ cp2_end_to_end_ml_project/main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from six.moves import urllib
 from zlib import crc32
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 #%%
@@ -29,6 +31,7 @@ fetch_housing_data()
 #%% load data
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
+
     return pd.read_csv(csv_path)
 
 
@@ -53,14 +56,33 @@ def test_set_check(identifier, test_ratio):
 
 
 def split_train_test_by_id(data, test_ratio, id_column):
-    ids = data(id_column)
+    ids = data[id_column]
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
     return data.loc[~in_test_set], data.loc[in_test_set]
 
-
+#%%
 housing_with_id = housing.reset_index()
-train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+# train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
 
 
 housing_with_id['id'] = housing['longitude'] * 1000 + housing['latitude']
 train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, 'id')
+
+
+#%% Strat
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+#%%
+housing["income_cat"].hist()
+plt.show()
+
+#%%
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+#%% Drop income_cat
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
