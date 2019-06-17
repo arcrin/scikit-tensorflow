d23f39090d5bc32192a452683ef14bcd61f70232
arcia <andy-wen@hotmail.com>
2019-06-17 00:05:16 -0700

    cp3 classification added

--- cp2_end_to_end_ml_project/main.py
+++ cp2_end_to_end_ml_project/main.py
@@ -21,6 +21,8 @@ from sklearn.metrics import mean_squared_error
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from scipy import stats
 
 
 #%%
@@ -273,3 +275,48 @@ forest_reg_rmse = np.sqrt(tree_mse)
 forest_reg_scores = cross_val_score(forest_reg, housing_prepared, housing_labels,
                                     scoring="neg_mean_squared_error", cv=10)
 forest_reg_rmse_scores = np.sqrt(-forest_reg_scores)
+
+
+#%% Fine-Tune Your Model, Grid Search
+param_grid = [
+    {"n_estimators": [3, 10, 30], "max_features": [2, 4, 6, 8]},
+    {"bootstrap": [False], "n_estimators": [3, 10], "max_features": [2, 3, 4]},
+]
+
+forest_reg = RandomForestRegressor()
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring="neg_mean_squared_error", return_train_score=True)
+
+grid_search.fit(housing_prepared, housing_labels)
+
+#%%
+cvres = grid_search.cv_results_
+for mean_score, params, in zip(cvres["mean_test_score"], cvres['params']):
+    print(np.sqrt(-mean_score), params)
+
+
+#%%
+feature_importances = grid_search.best_estimator_.feature_importances_
+extra_atribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+cat_encoder = full_pipeline.named_transformers_["cat"]
+cat_one_hot_attribs = list(cat_encoder.categories_[0])
+attributes = num_attribs + extra_atribs + cat_one_hot_attribs
+
+#%% Evaluating System on the Test Set
+final_model = grid_search.best_estimator_
+X_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+
+X_test_prepared = full_pipeline.transform(X_test)
+
+final_predictions = final_model.predict(X_test_prepared)
+
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
+
+
+#%%
+confidence = 0.95
+squared_errors = (final_predictions - y_test) ** 2
+temp = np.sqrt(stats.t.interval(confidence, len(squared_errors) - 1,
+                         loc=squared_errors.mean(),
+                         scale=stats.sem(squared_errors)))
---
+++ cp3_classification/main.py
@@ -0,0 +1,143 @@
+#%%
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+from sklearn.model_selection import cross_val_score
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#%%
+mnist = fetch_openml("mnist_784", version=1)
+
+#%%
+x, y = mnist["data"], mnist["target"]
+
+#%%
+some_digit = x[0]
+some_digit_image = some_digit.reshape(28, 28)
+
+plt.imshow(some_digit_image, cmap="binary")
+plt.axis("off")
+plt.show()
+
+
+#%%
+y = y.astype(np.uint8)
+
+#%% training and test set
+x_train, x_test, y_train, y_test = x[:60000], x[60000:], y[:60000], y[60000:]
+
+#%% Binary classifier, 5-detector
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+#%%
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(x_train, y_train_5)
+
+
+#%% Performance, Measuring Accuracy using Cross-Validation
+skfolds = StratifiedKFold(n_splits=3, random_state=42)
+
+for train_index, test_index in skfolds.split(x_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    x_train_folds = x_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    x_test_fold = x_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(x_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(x_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+
+#%%
+cross_val_score(sgd_clf, x_train, y_train_5, cv=3, scoring="accuracy")
+
+#%% Dumb classifier
+
+class Never5Classifier(BaseEstimator):
+    def fit(self, x, y=None):
+        pass
+
+    def predict(self, x):
+        return np.zeros((len(x), 1), dtype=bool)
+
+#%%
+never_5_clf = Never5Classifier()
+print(cross_val_score(never_5_clf, x_train, y_train_5, cv=3, scoring="accuracy"))
+
+
+#%% Confusion Matrix
+y_train_pred = cross_val_predict(sgd_clf, x_train, y_train_5, cv=3)
+res = confusion_matrix(y_train_5, y_train_pred)
+
+#%%
+y_train_perfect_predictions = y_train_5
+res = confusion_matrix(y_train_5, y_train_perfect_predictions)
+
+#%% Precision and Recall
+precision_score_res = precision_score(y_train_5, y_train_pred)
+recall_score_res = recall_score(y_train_5, y_train_pred)
+
+#%% F1 score
+f1_score_res = f1_score(y_train_5, y_train_pred)
+
+#%% Precision/Recall Trade off
+y_scores = sgd_clf.decision_function([some_digit])
+threshold = 0
+y_some_digit_pred = (y_scores > threshold)
+
+#%%
+threshold = 8000
+y_some_digit_pred = (y_scores > threshold)
+
+#%% Picking threshold
+y_scores = cross_val_predict(sgd_clf, x_train, y_train_5, cv=3, method="decision_function")
+
+#%% Precision Recall curve
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+
+#%%
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+    plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+    plt.legend()
+
+#%%
+threshold_90_precision = thresholds[np.argmax(precisions >= 0.90)]
+y_train_pred_90 = (y_scores >= threshold_90_precision)
+
+
+#%%
+precision_score(y_train_5, y_train_pred_90)
+
+#%%
+recall_score(y_train_5, y_train_pred_90)
+
+#%% The ROC (receiver operating characteristic) Curve
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    if label is not None:
+        plt.legend()
+
+#%%
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+plot_roc_curve(fpr, tpr)
+
+#%% ROC AUC
+roc_auc_score(y_train_5, y_scores)
