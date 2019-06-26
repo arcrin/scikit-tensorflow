c2f8be13bd306f2274c0b92e4786b51e2bf44ab9
ivan <ichichkine@deltacontrols.com>
2019-06-25 18:16:57 -0700

    from work

--- cp4-training models/main.py
+++ cp4-training models/main.py
@@ -38,3 +38,88 @@ theta_best_svd, residuals, rank, s = np.linalg.lstsq(x_b, y, rcond=1e-6)
 
 #%% Pseudoinverse
 np.linalg.pinv(x_b).dot(y)
+
+#%% Linear regression using batch gradient descent
+eta = 0.1  # learning rate
+n_iterations = 1000
+m = 100
+
+theta = np.random.randn(2, 1)
+
+for iteration in range(n_iterations):
+    gradients = 2/m * x_b.T.dot(x_b.dot(theta) - y)
+    theta = theta - eta * gradients
+
+print(theta)
+
+#%%
+x_new_b.dot(theta)
+
+#%%
+theta_path_bgd = []
+
+
+def plot_gradient_descent(theta, eta, theta_path=None):
+    m = len(x_b)
+    plt.plot(x, y, "b.")
+    n_iterations = 1000
+    for iteration in range(n_iterations):
+        if iteration < 10:
+            y_predict = x_new_b.dot(theta)
+            style = "b-" if iteration > 0 else "r--"
+            plt.plot(x_new, y_predict, style)
+        gradients = 2/m * x_b.T.dot(x_b.dot(theta) - y)
+        theta = theta - eta * gradients
+        if theta_path is not None:
+            theta_path.append(theta)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 2, 0, 15])
+    plt.title(r"$\eta = {}$".format(eta), fontsize=16)
+
+#%%
+np.random.seed(42)
+theta = np.random.randn(2, 1)  # random initialization
+
+plt.figure(figsize=(10, 4))
+plt.subplot(131); plot_gradient_descent(theta, eta=0.02)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(132); plot_gradient_descent(theta, eta=0.1, theta_path=theta_path_bgd)
+plt.subplot(133); plot_gradient_descent(theta, eta=0.5)
+
+plt.show()
+
+#%% Stochastic Gradient Descent
+theta_path_sgd = []
+m = len(x_b)
+np.random.seed(42)
+
+#%%
+n_epoches = 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
+
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+
+theta = np.random.randn(2, 1)  # random initialization
+
+for epoch in range(n_epoches):
+    for i in range(m):
+        if epoch == 0 and i < 20:
+            y_predict = x_new_b.dot(theta)
+            style = "b-" if i > 0 else "r--"
+            plt.plot(x_new, y_predict, style)
+        random_index = np.random.randint(m)
+        xi = x_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+        theta_path_sgd.append(theta)
+
+plt.plot(x, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([0, 2, 0, 15])
+plt.show()
